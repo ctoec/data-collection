e950dc459512791636916c239fd4d2309eb7f537
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-07-08 16:44:50 -0400

    Fix snapshot tests

--- client/src/containers/App/App.test.tsx
+++ client/src/containers/App/App.test.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 it('matches snapshot', () => {
-  const { asFragment } = render(<App />);
+  const { asFragment } = render(
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	);
   expect(asFragment()).toMatchSnapshot();
 });
