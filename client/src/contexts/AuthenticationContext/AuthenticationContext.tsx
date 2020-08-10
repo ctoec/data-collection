e6addc8c99f58e0a3bb3496be38ce5ac6cabb580
Jordan Guinn <jordan@skylight.digital>
2020-08-10 16:01:56 -0700

    Let's try this

--- client/src/contexts/AuthenticationContext/AuthenticationContext.tsx
+++ client/src/contexts/AuthenticationContext/AuthenticationContext.tsx
@@ -126,6 +126,8 @@ const AuthenticationProvider: React.FC<AuthenticationProviderPropsType> = ({
       if (!!defaultOpenIdConnectUrl) {
         setOpenIdConnectUrl(defaultOpenIdConnectUrl);
       } else if (!openIdConnectUrl) {
+        console.log('AHAHAHA', process.env.REACT_APP_WINGED_KEYS_HOST);
+
         const wingedKeysUri = `${process.env.REACT_APP_WINGED_KEYS_HOST || 'https://winged-keys:5050/'}`;
         if (!wingedKeysUri) {
           throw new Error('No winged keys uri found');
