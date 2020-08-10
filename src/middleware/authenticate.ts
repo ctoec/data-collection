120a64ad556542223c593e57f4c819d0d84cf57b
Jordan Guinn <jordan@skylight.digital>
2020-08-10 15:05:58 -0700

    protocol and slash added to env

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -8,7 +8,7 @@ import jwks from 'jwks-rsa';
 import { UserService } from '../services/user/UserService';
 
 const jwksClient = jwks({
-  jwksUri: `'https://'${(process.env.WINGED_KEYS_HOST || 'localhost:5050')}/.well-known/openid-configuration/jwks`,
+  jwksUri: `${(process.env.WINGED_KEYS_HOST || 'https://localhost:5050/')}.well-known/openid-configuration/jwks`,
   // @TODO Add HTTPS support
   strictSsl: false,
 });
