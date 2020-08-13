319325a7bbf39d259858adf0658116c2c2a6eb61
Jordan Guinn <jordan@skylight.digital>
2020-08-13 11:50:43 -0700

    Now we've got everything except the huge logo with a check

--- client/src/containers/SubmitSuccess/SubmitSuccess.tsx
+++ client/src/containers/SubmitSuccess/SubmitSuccess.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { MailToLink } from '../../components/MailToLink';
-import { Button } from '@ctoec/component-library';
+import { Button, TextWithIcon } from '@ctoec/component-library';
 import { CSVDownloadLink } from '../../components/CSVDownloadLink';
+import { ReactComponent as Arrow } from '@ctoec/component-library/dist/assets/images/arrowRight.svg';
+import { Link } from 'react-router-dom';
+
 
 const SubmitSuccess: React.FC = () => {
   return (
     <div className="grid-container margin-top-4">
+      <div className="grid-row margin-bottom-2 text-bold">
+          <Link to="/check-data">
+            <TextWithIcon
+              text="Back"
+              Icon={Arrow}
+              direction="left"
+              iconSide="left"
+            />
+          </Link>
+        </div>
       <div>
         <h1>You've submitted your data to OEC!</h1>
         <h5 className="margin-top-1">Date submitted: {new Date().toLocaleDateString()}</h5>
