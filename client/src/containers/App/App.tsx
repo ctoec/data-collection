26aa79844439006f0b71feb2f52a16ca7e764126
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-08-13 14:54:16 -0400

    Add SimpleHeader

--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -1,29 +1,52 @@
 import React, { useContext } from 'react';
 import { Switch } from 'react-router-dom';
-import { Header } from '@ctoec/component-library';
+import { SimpleHeader } from '@ctoec/component-library';
 
 import routes from '../../routes';
 import MakeRouteWithSubRoutes from './MakeRouteWithSubroute';
 import UserContext from '../../contexts/UserContext/UserContext';
+import { MailToLink } from '../../components/MailToLink';
 
 const App: React.FC = () => {
   const { user } = useContext(UserContext);
   return (
     <div className="App">
-      <Header
-        secondaryTitle="Early childhood enrollment data"
+			<SimpleHeader
+				secondaryTitle="Early childhood enrollment data"
         primaryTitle="Office of Early Childhood"
         loginPath="/login"
         logoutPath="/logout"
         navItems={[
+					{
+						id: 'templates-nav',
+            title: 'Data template',
+            children: [
+							{
+								id: 'excel-template-nav',
+								title: 'Excel',
+								path: "/upload_template/ECE Data Collection Template.xlsx"
+							},
+							{
+								id: 'csv-template-nav',
+								title: '.csv',
+								path: "/upload_template/ECE Data Collection Template.csv",
+							},
+						],
+						renderer: (props) => <a {...props} onClick={undefined} href={props.value}>{props.text}</a>
+					},
+					{
+						id: 'data-definitions-nav',
+            title: 'Data definitions',
+						path: '/data-definitions',
+					},
           {
-            type: 'secondary',
-            title: 'Privacy policy',
-            path: '/privacy-policy',
+						id: 'feedback-nav',
+            title: 'Feedback',
+						renderer: () => <MailToLink text='Feedback' />
           },
         ]}
         userFirstName={user?.firstName}
-      />
+			/>
       <Switch>
         {routes.map((route, index) => (
           <MakeRouteWithSubRoutes key={index} {...route} />
