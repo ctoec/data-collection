ae69c9d7aeb3eaa2dbd6eeda2a3fe9aa720db2a3
Jordan Guinn <jordan@skylight.digital>
2020-08-13 12:12:12 -0700

    Add a bit more to the TODO

--- client/src/components/CSVDownloadLink.tsx
+++ client/src/components/CSVDownloadLink.tsx
@@ -6,6 +6,8 @@ export const CSVDownloadLink = () => (
   <Button
     appearance="unstyled"
     //  TODO: Have this actual download uploaded data, not the upload template
+    //  We'll probably need to generate a CSV on the fly, since I don't think we're
+    //  holding uploaded files indefinitely?
     href="/upload_template/ECE Data Collection Template.csv"
     className="text-bold margin-bottom-3 display-block"
     external
