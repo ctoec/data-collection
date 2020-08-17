c4b0fdeffb3ed335dcf32743c5af7c8824d13447
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-08-17 10:08:20 -0400

    PR comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import { authenticate } from '../middleware/authenticate';
 import { router as userRouter } from './user';
 import { router as enrollmentReportRouter } from './enrollmentReport';
-import { router as enrollmentReportDownloadRouter } from './enrollmentReportDownload';
 import { router as dataDefinitionRouter } from './dataDefinition';
 
 export const router = express.Router();
@@ -14,8 +13,3 @@ router.use('/data-definitions', dataDefinitionRouter);
 /* AUTHENTICATED ROUTES */
 router.use('/users', authenticate, userRouter);
 router.use('/enrollment-reports', authenticate, enrollmentReportRouter);
-router.use(
-  '/enrollment-reports/download',
-  authenticate,
-  enrollmentReportDownloadRouter
-);
