03f227ebf1fba3558dab557eb440437b3ee98e2f
bamader <brandon@skylight.digital>
2020-08-18 11:28:22 -0400

    Entity implements shared interface

--- src/entity/Community.ts
+++ src/entity/Community.ts
@@ -1,15 +1,19 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 import { Organization } from './Organization';
+import { Community as CommunityInterface } from '../../shared/models';
 
 @Entity()
-export class Community {
+export class Community implements CommunityInterface {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
 
-  @OneToMany((type) => Organization, (Organization) => Organization.communityId)
+  @OneToMany(
+    (type) => Organization, 
+    (Organization) => Organization.communityId
+  )
   organizations?: Array<Organization>;
 }
