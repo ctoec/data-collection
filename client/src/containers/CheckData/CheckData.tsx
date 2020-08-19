bf71fc883092405356e4847b54ec6741071b9dfb
jlhogan <julia@skylight.digital>
2020-08-19 07:19:21 -0400

    148: Fast follow fixes for data table (#155)
    * 148: Fast follow fixes for data table
    
    also improves the UX for file upload by removing buttons
    
    * Use library for horizontal scroll bar
    
    * Combine imports from same library
    
    Co-authored-by: Melanie <melanie.mazanec@gmail.com>

--- client/src/containers/CheckData/CheckData.tsx
+++ client/src/containers/CheckData/CheckData.tsx
@@ -1,16 +1,20 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import pluralize from 'pluralize';
-
+import moment from 'moment';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import AuthenticationContext from '../../contexts/AuthenticationContext/AuthenticationContext';
 import { FlattenedEnrollment, DataDefinitionInfo } from 'shared/models';
 import { apiGet } from '../../utils/api';
 import {
   TextWithIcon,
-  EditableTable,
-  EditableTableColumn,
   Button,
+  Table,
+  Column,
+  TextInput,
+  ExpandRow,
 } from '@ctoec/component-library';
 import { ReactComponent as Arrow } from '@ctoec/component-library/dist/assets/images/arrowRight.svg';
 
@@ -41,46 +45,98 @@ const CheckData: React.FC = () => {
     }
   }, [reportId, accessToken]);
 
-  const tableColumns: EditableTableColumn<
-    FlattenedEnrollment
-  >[] = columnMetadata.map((columnMeta) => ({
-    title: columnMeta.formattedName,
-    field: columnMeta.propertyName,
-    validate: (rowData: any) => rowData[columnMeta.propertyName] !== '',
-    render: (rowData: any) => {
-      const cellData = rowData[columnMeta.propertyName];
-      if (cellData instanceof Date) return cellData.toLocaleDateString();
-      if (typeof cellData === 'boolean') return cellData ? 'yes' : 'no';
-      return cellData;
-    },
-  }));
+  const tableColumns: Column<FlattenedEnrollment>[] = columnMetadata.map(
+    (columnMeta) => ({
+      className: 'text-pre text-center',
+      name: columnMeta.formattedName,
+      title: columnMeta.propertyName,
+      cell: ({ row }) => {
+        // special case for clickable name column that expands row
+        if (columnMeta.propertyName === 'name') {
+          return (
+            <td>
+              <ExpandRow>
+                <Button
+                  className="text-no-wrap"
+                  appearance="unstyled"
+                  text={row.name || ''}
+                />
+              </ExpandRow>
+            </td>
+          );
+        }
+
+        // all other columns
+        let cellContent: any = (row as any)[columnMeta.propertyName];
+        if (typeof cellContent === 'boolean') {
+          cellContent = cellContent ? 'yes' : 'no';
+        } else if (moment.isMoment(cellContent)) {
+          cellContent = cellContent.format('MM/DD/YYYY');
+        }
+
+        return <td className="font-body-2xs">{cellContent}</td>;
+      },
+    })
+  );
 
   return (
     <>
-      <div className="CheckData__content grid-container margin-top-4">
-        <div className="margin-bottom-2 text-bold">
-          <Link to="/">
-            <TextWithIcon
-              text="Back"
-              Icon={Arrow}
-              direction="left"
-              iconSide="left"
-            />
-          </Link>
-        </div>
-        <h1>Check data for {pluralize('child', reportData.length, true)}</h1>
-        <p>Make sure all of your data was uploaded correctly. </p>
-        <p>If everything looks good, submit to OEC.</p>
-        {reportData.length && (
-          <EditableTable<FlattenedEnrollment>
-            title="Enrollment report table"
-            data={reportData}
-            columns={tableColumns}
+      <div className="CheckData__content margin-top-4">
+        <div className="margin-x-4">
+          <Button
+            className="margin-bottom-2 text-bold"
+            appearance="unstyled"
+            text={
+              <TextWithIcon
+                text="Back"
+                Icon={Arrow}
+                direction="left"
+                iconSide="left"
+              />
+            }
+            href="/upload"
           />
-        )}
+          <h1>Check data for {pluralize('child', reportData.length, true)}</h1>
+          <p>Make sure all of your data was uploaded correctly. </p>
+          <p>If everything looks good, submit to OEC.</p>
+          {reportData.length ? (
+            <PerfectScrollbar>
+              <Table<FlattenedEnrollment>
+                id="enrollment-report-table"
+                rowKey={(row) => row.id}
+                data={reportData}
+                columns={tableColumns}
+                rowExpansionRender={(row) => (
+                  <div className="grid-row flex-row">
+                    <div className="grid-col-1">
+                      <h3>Edit the stuff about {row.name} </h3>
+                      <TextInput
+                        label="An input can go here"
+                        id="text-input"
+                        type="input"
+                        onChange={() => {}}
+                      />
+                      <Button
+                        className="margin-top-2"
+                        text="A button can go here"
+                      />
+                    </div>
+                    <div className="grid-col-1">
+                      <ExpandRow>
+                        <Button text="CLOSE THIS EXPANSION" />
+                      </ExpandRow>
+                    </div>
+                  </div>
+                )}
+              />
+            </PerfectScrollbar>
+          ) : (
+            'Loading...'
+          )}
+        </div>
       </div>
       <div className="CheckData__button-container position-fixed bottom-0 width-full">
-        <div className="grid-container margin-bottom-0">
+        <div className="margin-bottom-0">
           <div className="fixed-buttons">
             <Button text="Back to upload" href="/upload" appearance="outline" />
             <Button text="Send to OEC" href="/success" />
