d529173d8a3f9a086cc61e8a476e7c02313c75e7
Melanie <melanie.mazanec@gmail.com>
2020-08-20 14:17:54 -0400

    Correct mailto link

--- client/src/components/MailToLink.tsx
+++ client/src/components/MailToLink.tsx
@@ -4,7 +4,7 @@ export const MailToLink = ({ text }: { text?: string }) => (
   // TODO: MAKE THE BODY CONTAIN USEFUL INFORMATION LIKE URL?
   // TODO: MAKE THIS SEPARATE COMPONENT IN COMPONENT LIBRARY?
   <a
-    href="mailto:oec-data-pilot@skylight.digital&subject=ECE%20reporter%20bug"
+    href="mailto:oec-data-pilot@skylight.digital?subject=Data%20collection%20bug"
     target="_blank"
     rel="noopener noreferrer"
   >
