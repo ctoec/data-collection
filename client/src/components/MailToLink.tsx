de9eac1959ca2aec7947f5b35af1c132ee28c507
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2020-09-03 12:25:57 -0400

    Component lib changes (#264)
    * Update component library and make template download link nav bar version
    
    * Mailto link

--- client/src/components/MailToLink.tsx
+++ client/src/components/MailToLink.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
+export const mailToLinkProps = {
+  href:
+    'mailto:oec-data-pilot@skylight.digital?subject=Data%20collection%20bug',
+  target: '_blank',
+  rel: 'noopener noreferrer',
+};
+
 export const MailToLink = ({ text }: { text?: string }) => (
-  // TODO: MAKE THE BODY CONTAIN USEFUL INFORMATION LIKE URL?
-  // TODO: MAKE THIS SEPARATE COMPONENT IN COMPONENT LIBRARY?
-  <a
-    href="mailto:oec-data-pilot@skylight.digital?subject=Data%20collection%20bug"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    {text || 'oec-data-pilot@skylight.digital'}
-  </a>
+  <a {...mailToLinkProps}>{text || 'oec-data-pilot@skylight.digital'}</a>
 );
