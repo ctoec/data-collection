e58684e1f9247c9bc12aae112706eefb24faa134
bamader <brandon@skylight.digital>
2020-09-09 13:57:50 -0400

    Initial CSVDownloadLink modification to remove href

--- client/src/components/CSVDownloadLink.tsx
+++ client/src/components/CSVDownloadLink.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Button, TextWithIcon, DownloadArrow } from '@ctoec/component-library';
+import { downloadStreamToFile } from '../utils/fileDownload';
 
-export const CSVDownloadLink = () => (
-  <Button
-    appearance="unstyled"
-    //  TODO: Have this actual download uploaded data, not the upload template
-    //  We'll probably need to generate a CSV on the fly, since I don't think we're
-    //  holding uploaded files indefinitely?
-    href="/upload_template/ECE Data Collection Template.csv"
-    className="text-bold margin-bottom-3 display-block"
-    external
-    text={
-      <TextWithIcon
-        text="Download .csv"
-        Icon={DownloadArrow}
-        iconSide="left"
-        className="text-underline"
-      />
-    }
-  />
-);
+export const CSVDownloadLink: React.FC = () => {
+  async function downloadTemplate() {
+    await downloadStreamToFile(
+      `column-metadata/csv`,
+      `ECE Data Collection Template.csv`
+    );
+  }
+
+  return (
+    <Button
+      appearance="unstyled"
+      //  TODO: Have this actual download uploaded data, not the upload template
+      //  We'll probably need to generate a CSV on the fly, since I don't think we're
+      //  holding uploaded files indefinitely?
+      onClick={downloadTemplate}
+      className="text-bold margin-bottom-3 display-block"
+      external
+      text={
+        <TextWithIcon
+          text="Download.csv"
+          Icon={DownloadArrow}
+          iconSide="left"
+          className="text-underline"
+        />
+      }
+    />
+  );
+};
