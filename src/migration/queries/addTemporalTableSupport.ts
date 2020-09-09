51c0b0dc605799ef0b40e09f2173eef4620c5f99
jlhogan <julia@skylight.digital>
2020-09-09 19:36:22 -0400

    Adds temporal table support and turns system versioning on for funding, enrollment, family, incomedetermination, child

--- src/migration/queries/addTemporalTableSupport.ts
+++ src/migration/queries/addTemporalTableSupport.ts
@@ -5,7 +5,7 @@ export const addHistorySchemaQuery = () => `
 `;
 
 // from https://docs.microsoft.com/en-us/sql/relational-databases/tables/creating-a-system-versioned-temporal-table?view=sql-server-ver15#adding-versioning-to-non-temporal-tables
-export const addPeriodColumnsQuery = (tableName: string) => `
+export const addSystemVersioningColumnsToTableQuery = (tableName: string) => `
 	ALTER TABLE ${tableName} ADD
 		SysStartTime DATETIME2(0) GENERATED ALWAYS AS ROW START HIDDEN
 			CONSTRAINT DF_${tableName}_SysStart DEFAULT SYSUTCDATETIME(),
@@ -13,7 +13,7 @@ export const addPeriodColumnsQuery = (tableName: string) => `
 			CONSTRAINT DF_${tableName}_SysEnd DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59'),
 		PERIOD FOR SYSTEM_TIME(SysStartTime, SysEndTime)`;
 
-export const addHistoryTableQuery = (tableName: string) => `
+export const turnSystemVersioningOnForTableQuery = (tableName: string) => `
 	ALTER TABLE ${tableName}
 		SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = ${HISTORY_SCHEMA}.${tableName}))
 `;
