2e11bac955ed749e698a5c6407f45fd01a5e13a3
bamader <brandon@skylight.digital>
2020-09-09 16:26:30 -0400

    Trying to turn Child[] into a 2D string array to stuff in a sheet

--- src/controllers/export.ts
+++ src/controllers/export.ts
@@ -1,21 +1,37 @@
 import { write, WorkBook, utils, ColInfo, WorkSheet } from 'xlsx';
 import { ColumnMetadata } from '../../client/src/shared/models';
-import { wrapText } from '../utils/string';
-import { EntityMetadata, getConnection } from 'typeorm';
-import { FlattenedEnrollment } from '../entity';
+import { EntityMetadata, getConnection, getManager } from 'typeorm';
+import { FlattenedEnrollment, Child } from '../entity';
 import { getColumnMetadata } from '../entity/decorators/columnMetadata';
 import { Response } from 'express';
+import { format } from 'path';
 
-export function streamTemplate(response: Response) {
-  const template: WorkBook = generateCsvWorkbook();
+export async function streamUploadedChildren(
+  response: Response,
+  childIds: string[]
+) {
+  var childrenToMap: Child[] = [];
+  childIds.forEach(async (id) => {
+    childrenToMap.push(await getManager().findOne(Child, { id: id }));
+  });
 
-  const templateStream = write(template, {
+  const csvToExport: WorkBook = generateCSV(childrenToMap);
+
+  const csvStream = write(csvToExport, {
     bookType: 'csv',
     type: 'buffer',
   });
   response.contentType('application/octet-stream');
-  response.send(templateStream);
+  response.send(csvStream);
 }
+
+// export async function retrieveChildren(childIds: string[]) {
+//     var childrenToMap: Child[] = [];
+//     childIds.forEach(async (id) => {
+//         childrenToMap.push(await getManager().findOne(Child, {id: id}));
+//     });
+//     return childrenToMap;
+// }
 
 /**
  * Retrieve our own custom ColumnMetadata for all columns on the FlattenedEnrollment model.
@@ -32,16 +48,23 @@ export function getAllEnrollmentColumns(): ColumnMetadata[] {
     .filter((templateMeta) => !!templateMeta);
 }
 
-function generateCsvWorkbook(): WorkBook {
-  const columnMetadatas: ColumnMetadata[] = getAllEnrollmentColumns();
+function flattenChild(child: Child, cols: ColumnMetadata[]) {
+  return cols.map((colName) => {
+    child[colName.propertyName];
+  });
+  // Object.keys(child)
+}
 
+function generateCSV(childArray: Child[]): WorkBook {
+  const columnMetadatas: ColumnMetadata[] = getAllEnrollmentColumns();
   const formattedColumnNames: string[] = columnMetadatas.map(
     (c) => c.formattedName
   );
+  const childStrings = childArray.map((c) => flattenChild(c, columnMetadatas));
   const sheet = utils.aoa_to_sheet([formattedColumnNames]);
-
   const workbook = utils.book_new();
   utils.book_append_sheet(workbook, sheet);
-
+  const children = utils.aoa_to_sheet(childStrings);
+  utils.book_append_sheet(workbook, children);
   return workbook;
 }
--- src/routes/export.ts
+++ src/routes/export.ts
@@ -1,9 +1,23 @@
 import express from 'express';
 import { Response, Request } from 'express';
 import * as controller from '../controllers/export';
+import { passAsyncError } from '../middleware/error/passAsyncError';
 
 export const exportRouter = express.Router();
 
-exportRouter.get('/csv-upload', (req: Request, res: Response) => {
-  res.send(controller.streamTemplate(res));
-});
+exportRouter.get(
+  '/csv-upload/:idString',
+  // (req: Request, res: Response) => {
+  // res.send(controller.streamTemplate(res));
+  passAsyncError(async (req: Request, res: Response) => {
+    try {
+      const idString = req.params['idString'];
+      const uploadedIds = idString.split(',');
+      // const children = await controller.retrieveChildren(uploadedIds);
+      res.send(controller.streamUploadedChildren(res, uploadedIds));
+    } catch (err) {
+      console.error('BADNESS');
+    }
+  })
+  // });
+);
