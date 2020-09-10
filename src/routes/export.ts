256d41e8ccca015b18c613e86d83b4bfe2bed684
bamader <brandon@skylight.digital>
2020-09-10 17:02:14 -0400

    Why are headers changing themselves

--- src/routes/export.ts
+++ src/routes/export.ts
@@ -9,26 +9,12 @@ export const exportRouter = express.Router();
 
 exportRouter.get(
   '/csv-upload/:idString',
-  // (req: Request, res: Response) => {
-  // res.send(controller.streamTemplate(res));
   passAsyncError(async (req: Request, res: Response) => {
     try {
       const idString = req.params['idString'];
       const uploadedIds = idString.split(',');
-      // res.send(uploadedIds);
-      var childrenToMap: Child[] = [];
-      for (let i = 0; i < uploadedIds.length; i++) {
-        childrenToMap.push(
-          await getManager().findOne(Child, { id: uploadedIds[i] })
-        );
-      }
-      // uploadedIds.forEach(async (id) => {
-      // childrenToMap.push(await getManager().findOne(Child, { id: id }));
-      // });
-      // childrenToMap.push(await getManager().findOne(Child, {id: uploadedIds[0]}));
-      // res.send(childrenToMap);
-      // const workbook = controller.streamUploadedChildren(res, childrenToMap);
-      res.send(controller.streamUploadedChildren(res, childrenToMap));
+      res.send(idString);
+      // res.send(controller.streamUploadedChildren(res, uploadedIds));
     } catch (err) {
       console.error('BADNESS');
     }
