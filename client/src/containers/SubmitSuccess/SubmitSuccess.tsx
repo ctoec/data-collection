b757be24e821aafc822d197377cdaf398296a00a
bamader <49412165+bamader@users.noreply.github.com>
2020-09-14 08:48:19 -0400

    Remove double import

--- client/src/containers/SubmitSuccess/SubmitSuccess.tsx
+++ client/src/containers/SubmitSuccess/SubmitSuccess.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import moment from 'moment';
-import { useParams } from 'react-router-dom';
 import { MailToLink } from '../../components/MailToLink';
 import {
   Button,
