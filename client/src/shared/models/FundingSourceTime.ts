497b762b1ee20a904adce96b2c02efe96eb2d4aa
Jordan Guinn <jordan@skylight.digital>
2020-09-15 11:38:58 -0700

    Move the thing

--- client/src/shared/models/FundingSourceTime.ts
+++ client/src/shared/models/FundingSourceTime.ts
@@ -1,8 +1,14 @@
 import { FundingSource } from ".";
-import { FundingTimeInput } from "./FundingTime";
+import { FundingTime } from "./FundingTime";
 
 export interface FundingSourceTime {
   displayName: string;
   fundingSources: FundingSource[];    //  All FundingSources that can have the associated FundingTimes
   fundingTimes: FundingTimeInput[];   //  All valid FundingTimes for the associated FundingSources 
+}
+
+export interface FundingTimeInput {
+  displayName: string;  //  A user-friendly version of this funding time
+  formats: string[];    //  All accepted input formats for this funding time
+  value: FundingTime;   //  The actual underlying funding time
 }
