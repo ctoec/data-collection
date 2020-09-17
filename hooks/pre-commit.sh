38aa1be61a07fc04a18fc8ff7dd19c51a31f52c3
Jordan Guinn <jordan@skylight.digital>
2020-09-17 12:51:30 -0700

    lol

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -5,7 +5,7 @@ FILES=$(git diff --cached --name-only --diff-filter=ACMR "*.js" "*.jsx" "*.ts" "
 [ -z "$FILES" ] && exit 0
 
 cmd="yarn run prettier $FILES"
-docker-compose exec -T server $cmd && echo "\nPrettier formatting is complete.\n" || { echo "\nPrettier formatting failed - is your docker container up and running?\n"; exit 1 }
+docker-compose exec -T server $cmd && echo "\nPrettier formatting is complete.\n" || { echo "\nPrettier formatting failed - is your docker container up and running?\n"; exit 1; }
 
 echo "$FILES" | xargs git add
 
