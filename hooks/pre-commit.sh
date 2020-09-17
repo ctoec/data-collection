29e74ee8610a02bf77a0e68d10bbc840c3f7713c
Jordan Guinn <jordan@skylight.digital>
2020-09-17 12:46:18 -0700

    hello

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,13 +1,11 @@
 
 #!/bin/sh
 # Adapted from: https://prettier.io/docs/en/precommit.html#option-6-shell-script
-
 FILES=$(git diff --cached --name-only --diff-filter=ACMR "*.js" "*.jsx" "*.ts" "*.tsx" "*.json" "*.css" "*.scss" | sed 's| |\\ |g')
 [ -z "$FILES" ] && exit 0
 
-process.exit(1)
 cmd="yarn run prettier $FILES"
-docker-compose exec -T server $cmd
+docker-compose exec -T server $cmd && echo "\nPrettier formatting is complete.\n" || { echo "\nPrettier formatting failed - is your docker container up and running?\n"; exit 1 }
 
 echo "$FILES" | xargs git add
 
