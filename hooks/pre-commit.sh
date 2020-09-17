9d7c22e21877e44bd95c716c0b56921f766283df
Jordan Guinn <jordan@skylight.digital>
2020-09-17 12:18:07 -0700

    let's try this

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -5,6 +5,7 @@
 FILES=$(git diff --cached --name-only --diff-filter=ACMR "*.js" "*.jsx" "*.ts" "*.tsx" "*.json" "*.css" "*.scss" | sed 's| |\\ |g')
 [ -z "$FILES" ] && exit 0
 
+exit 1
 cmd="yarn run prettier $FILES"
 docker-compose exec -T server $cmd
 
