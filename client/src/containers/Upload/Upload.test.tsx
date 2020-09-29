415cfe7446cafc11a706216ffe6740ff35308854
jlhogan <julia@skylight.digital>
2020-09-29 16:01:22 -0400

    350: (PART I) Upload replaces roster - Front end
    TODO: Backend - make the server parse & use the `overwriteSite` id
    query params in enrollment report upload

--- client/src/containers/Upload/Upload.test.tsx
+++ client/src/containers/Upload/Upload.test.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { snapshotTestHelper, accessibilityTestHelper } from '../../testHelpers';
 import Upload from './Upload';
 
+jest.mock('../../utils/api');
+import * as api from '../../utils/api';
+const apiMock = api as jest.Mocked<typeof api>;
+
 describe('Upload', () => {
+  beforeAll(() => {
+    apiMock.apiGet.mockReturnValue(new Promise((resolve) => resolve(0)));
+  });
   snapshotTestHelper(<Upload />, { wrapInRouter: true });
   accessibilityTestHelper(<Upload />, { wrapInRouter: true });
 });
