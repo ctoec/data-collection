52348a5b371473bc7d798d762d894afe1d569a6d
Jordan Guinn <jordan@skylight.digital>
2020-09-29 22:38:41 -0700

    Easy enough

--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Switch } from 'react-router-dom';
-import { Header } from '@ctoec/component-library';
+import { Header, NavLink } from '@ctoec/component-library';
 
 import routes from '../../routes';
 import MakeRouteWithSubRoutes from './MakeRouteWithSubroute';
@@ -17,11 +17,21 @@ const App: React.FC = () => {
         Skip to main content
       </a>
       <Header
-        secondaryTitle="Early childhood enrollment data"
-        primaryTitle="Office of Early Childhood"
+        primaryTitle="ECE Reporter"
         loginPath="/login"
         logoutPath="/logout"
+        showPrimaryNavItems={!!user?.firstName}
         navItems={[
+          {
+            type: 'primary',
+            text: 'Roster',
+            path: '/roster',
+          },
+          {
+            type: 'primary',
+            text: 'Batch upload',
+            path: '/success', //  TODO: Actually route to batch upload when it's available
+          },
           {
             id: 'templates-nav',
             text: 'Data template',
@@ -52,6 +62,11 @@ const App: React.FC = () => {
         ]}
         userFirstName={user?.firstName}
       />
+      <div className="usa-nav__secondary usa-nav__secondary--extended">
+        <ul className="usa-nav__secondary-links">
+          <NavLink type="secondary" text="Log out" path="" />
+        </ul>
+      </div>
       <main id="main-content">
         <Switch>
           {routes.map((route, index) => (
