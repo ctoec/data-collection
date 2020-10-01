d9132449615fbc4aea7cad91407d6a21fc467ae5
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2020-10-01 17:33:55 -0400

    317 add record (#459)
    Update csv download link and snapshots

---
+++ client/src/components/AddRecordButton.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  ButtonWithDropdown,
+  PlusCircle,
+  TextWithIcon,
+} from '@ctoec/component-library';
+import { Organization } from '../shared/models';
+import { Link, useHistory } from 'react-router-dom';
+
+export const AddRecordButton = ({ orgs }: { orgs?: Organization[] }) => {
+  const history = useHistory();
+
+  if (!orgs) {
+    return <></>;
+  }
+
+  if (orgs.length === 1) {
+    return (
+      <Link
+        to={{
+          pathname: '/create-record',
+          state: { organization: orgs[0] },
+        }}
+      >
+        <TextWithIcon Icon={PlusCircle} text="Add a record" />
+      </Link>
+    );
+  }
+
+  return (
+    <ButtonWithDropdown
+      text={<TextWithIcon Icon={PlusCircle} text="Add a record" />}
+      id="select-organization"
+      options={orgs.map((p) => ({
+        text: p.providerName,
+        href: '/create-record',
+        onClick: () => {
+          history.push('/create-record', { state: { organization: p } });
+        },
+      }))}
+    />
+  );
+};
