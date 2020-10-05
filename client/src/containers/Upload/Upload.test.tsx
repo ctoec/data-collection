d8f7c6bc5973ecbeee971ded1343110662f5e65b
jlhogan <julia@skylight.digital>
2020-10-05 14:39:13 -0400

    Fixup tests

--- client/src/containers/Upload/Upload.test.tsx
+++ client/src/containers/Upload/Upload.test.tsx
@@ -4,12 +4,17 @@ import Upload from './Upload';
 
 jest.mock('../../utils/api');
 import * as api from '../../utils/api';
+import { waitFor } from '@testing-library/react';
 const apiMock = api as jest.Mocked<typeof api>;
 
+const helperOpts = {
+  wrapInRouter: true,
+  before: () => waitFor(() => expect(apiMock.apiGet).toBeCalled()),
+};
 describe('Upload', () => {
   beforeAll(() => {
     apiMock.apiGet.mockReturnValue(new Promise((resolve) => resolve(0)));
   });
-  snapshotTestHelper(<Upload />, { wrapInRouter: true });
-  accessibilityTestHelper(<Upload />, { wrapInRouter: true });
+  snapshotTestHelper(<Upload />, helperOpts);
+  accessibilityTestHelper(<Upload />, helperOpts);
 });
