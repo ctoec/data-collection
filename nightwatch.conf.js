cdf935d509e5d9d2f49cb94df86bd6673c502c7c
Melanie <melanie.mazanec@gmail.com>
2020-10-13 11:31:23 -0400

    PR comments

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -9,15 +9,25 @@ https: nightwatch_config = {
   test_settings: {
     default: {
       desiredCapabilities: {
-        'browserstack.user': 'julia518',
-        'browserstack.key': 'Uq4nUNp12oP9oMoYquej',
+        // ADD TO CI
+        'browserstack.user': process.env.BROWSERSTACK_USER,
+        'browserstack.key': process.env.BROWSERSTACK_KEY,
         browserName: 'edge',
+      },
+    },
+    ie: {
+      desiredCapabilities: {
+        // ADD TO CI
+        'browserstack.user': process.env.BROWSERSTACK_USER,
+        'browserstack.key': process.env.BROWSERSTACK_KEY,
+        browserName: 'internet explorer',
       },
     },
   },
 };
 
 // Code to copy seleniumhost/port into test settings
+// We're only using one desired capability for now but we will be using more later
 for (var i in nightwatch_config.test_settings) {
   var config = nightwatch_config.test_settings[i];
   config['selenium_host'] = nightwatch_config.selenium.host;
