2dcb86bba5a3e79534bd0efe6e4c30abb16171bc
Melanie <melanie.mazanec@gmail.com>
2020-10-14 12:30:21 -0400

    Remove typo

--- e2e-tests/login.js
+++ e2e-tests/login.js
@@ -10,8 +10,8 @@ module.exports = {
     browser.assert.title('IdentityServer4');
 
     // Enter username and password
-    await browser.setValue('css-selector', '#Username', 'voldemort');
-    await browser.setValue('css-selector', '#Password', 'thechosenone');
+    await browser.setValue('css selector', '#Username', 'voldemort');
+    await browser.setValue('css selector', '#Password', 'thechosenone');
     await browser.click('xpath', "//*/button[contains(@value,'login')]");
 
     // Check to see if we logged in and the title has been changed
