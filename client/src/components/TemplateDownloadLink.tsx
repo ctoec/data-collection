2f6b90f5ea806fa68d0dc4cc67391839c5541c1f
Kyle Magida <kyle@omelas.co>
2020-10-19 15:10:28 -0400

    Change CSV

--- client/src/components/TemplateDownloadLink.tsx
+++ client/src/components/TemplateDownloadLink.tsx
@@ -23,7 +23,7 @@ export const TemplateDownloadLink = ({
   }
 
   if (nav) {
-    downloadText = type === 'csv' ? 'csv' : 'Excel';
+    downloadText = type === 'csv' ? 'CSV' : 'Excel';
 
     return (
       <Button
