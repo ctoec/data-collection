87575eb9acdb09df13cd4dc90b6b24db14ba9084
jlhogan <julia@skylight.digital>
2020-10-22 14:51:08 -0400

    570: AgeGroups render in consistent order

--- client/src/containers/Roster/rosterUtils.tsx
+++ client/src/containers/Roster/rosterUtils.tsx
@@ -137,13 +137,15 @@ export function getChildrenByAgeGroup(
   filteredChildren: Child[]
 ): ChildrenByAgeGroup {
   const childrenByAgeGroup: ChildrenByAgeGroup = {};
-  return filteredChildren.reduce((_byAgeGroup, _child) => {
+  // Filter all children into an object, keyed by age group
+  // with array of children for that agegroup as value
+  filteredChildren.reduce((_byAgeGroup, _child) => {
     const ageGroup = idx(_child, (_) => _.enrollments[0].ageGroup) || undefined;
     if (ageGroup) {
       if (!!!_byAgeGroup[ageGroup]) {
         _byAgeGroup[ageGroup] = [_child];
       } else {
-        // acc[ageGroup] is not _actually_ possibly undefined; checked in above if
+        // _byAgeGroup[ageGroup] is not _actually_ possibly undefined; checked in above if
         _byAgeGroup[ageGroup]?.push(_child);
       }
     } else {
@@ -155,6 +157,22 @@ export function getChildrenByAgeGroup(
     }
     return _byAgeGroup;
   }, childrenByAgeGroup);
+
+  // Sort object to have consistent section ordering, where NoAgeGroup will always
+  // be first if it exists, followed by AgeGroups as they are defined in the enum
+  const sortedByAgeGroup: ChildrenByAgeGroup = {};
+  if (
+    childrenByAgeGroup[NoAgeGroup] &&
+    childrenByAgeGroup[NoAgeGroup]?.length
+  ) {
+    sortedByAgeGroup[NoAgeGroup] = childrenByAgeGroup[NoAgeGroup];
+  }
+  Object.values(AgeGroup).forEach((ageGroup) => {
+    if (childrenByAgeGroup[ageGroup] && childrenByAgeGroup[ageGroup]?.length) {
+      sortedByAgeGroup[ageGroup] = childrenByAgeGroup[ageGroup];
+    }
+  });
+  return sortedByAgeGroup;
 }
 
 export function getAccordionItems(
