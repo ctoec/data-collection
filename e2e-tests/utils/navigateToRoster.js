c30b03550158a47355586e65b0d6476b2e62999f
Melanie <melanie.mazanec@gmail.com>
2020-10-23 12:23:13 -0400

    Delete inaccurate comment

--- e2e-tests/utils/navigateToRoster.js
+++ e2e-tests/utils/navigateToRoster.js
@@ -2,7 +2,6 @@ const { makeSureNavIsOpen } = require('./makeSureNavIsOpen');
 
 module.exports = {
   navigateToRoster: async function (browser) {
-    // Check to see if user is logged in and log in if they are not
     await makeSureNavIsOpen(browser);
     await browser.click('xpath', "//*/a[contains(@href,'/roster')]");
     await browser.waitForElementVisible('css selector', 'body');
