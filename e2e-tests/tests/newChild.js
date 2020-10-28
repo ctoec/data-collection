06435df9b7ff6f06fd20eb7db77ea9e8c4b18366
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2020-10-28 11:58:57 -0400

    More e2e (#626)
    File download and upload tests, child record create/edit/delete, e2e readme

---
+++ e2e-tests/tests/newChild.js
@@ -0,0 +1,67 @@
+const { login } = require('../utils/login');
+const { navigateToRoster } = require('../utils/navigateToRoster');
+
+// const childIdentFields = [
+//   {
+//     id: 'firstName',
+//     value: 'New child',
+//   },
+//   {
+//     id: 'lastName',
+//     value: 'From e2e test',
+//   },
+//   {
+//     id: 'dateOfBirth-picker-month',
+//     value: '10',
+//   },
+//   {
+//     id: 'dateOfBirth-picker-day',
+//     value: '10',
+//   },
+//   {
+//     id: 'dateOfBirth-picker-year',
+//     value: '2017',
+//   },
+//   // TODO: FIX ID FOR BIRTH CERT RADIO BUTTONS
+// ];
+// const childInfoFields = [
+//   {
+//     id: 'raceNotDisclosed',
+//     value: true,
+//   },
+//   {
+//     // TODO: fix radio button ids
+//     id: 'Unknown',
+//     // TODO: make enter form data func able to handle attribute changes
+//     change: {
+//       attribute: 'checked',
+//       value: 'true',
+//     },
+//     // TODO: gender-- need to indicate which option is selected
+//     // TODO: fix radio button ids for disability services, dual language, foster family
+//   },
+// ];
+// const familyAddressFields = [];
+// const familyIncomeFields = [];
+// const enrollmentFundingFields = [];
+
+module.exports = {
+  '@tags': ['child', 'new'],
+  newChild: async function (browser) {
+    // Initializes with the launch_url value set in config
+    await browser.init();
+    // Log in
+    await login(browser);
+    // Navigate to roster
+    await navigateToRoster(browser);
+    // Add child
+    await browser.click('xpath', `//*/a[contains(., 'Add a record')]`);
+    // TODO: deal with jwt expired error that happens when adding a child after being logged out
+    await browser.waitForElementVisible('body');
+    browser.assert.title('Add a child record');
+
+    // TODO: enter info-- need to fix radio button id stuff first
+    // TODO: expect that child to show up in the right place on the roster
+    browser.end();
+  },
+};
