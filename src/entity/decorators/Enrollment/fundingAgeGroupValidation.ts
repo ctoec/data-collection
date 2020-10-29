09677ce4ed2f975b6972555791984cfda9035c36
bamader <brandon@skylight.digital>
2020-10-29 15:59:59 -0400

    Add funding age group match validation

---
+++ src/entity/decorators/Enrollment/fundingAgeGroupValidation.ts
@@ -0,0 +1,27 @@
+import { registerDecorator, ValidationOptions } from 'class-validator';
+import { Enrollment } from '../../Enrollment';
+
+const ageGroupsMustAgree =
+  'Age groups of funding spaces must match the enrollments they are associated with.';
+
+export function FundingAgeGroupMatchesEnrollment(
+  validationOptions?: ValidationOptions
+): PropertyDecorator {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'ageGroupMatch',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: { message: ageGroupsMustAgree, ...validationOptions },
+      validator: {
+        validate(_, { object: enrollment }) {
+          const enrollmentAgeGroup = (enrollment as Enrollment).ageGroup;
+          const allFundings = (enrollment as Enrollment).fundings;
+          return allFundings.every(
+            (f) => f.fundingSpace.ageGroup === enrollmentAgeGroup
+          );
+        },
+      },
+    });
+  };
+}
