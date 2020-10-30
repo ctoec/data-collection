3b004d49cf4c8c5650f2e5e1cb244d86078741c8
Melanie <melanie.mazanec@gmail.com>
2020-10-30 18:19:00 -0400

    Add margins

--- client/src/containers/DevUtility/DevUtility.tsx
+++ client/src/containers/DevUtility/DevUtility.tsx
@@ -10,12 +10,14 @@ const DevUtility: React.FC = () => {
         <div className="desktop:grid-col-8">
           <h1 ref={h1Ref}>Templates and sample data for development</h1>
           <h2>Templates</h2>
-          <CSVExcelDownloadButton fileType="xlsx" whichDownload="template" />
-          <CSVExcelDownloadButton fileType="csv" whichDownload="template" />
+          <CSVExcelDownloadButton fileType="xlsx" whichDownload="template" className="margin-bottom-3" />
+          <CSVExcelDownloadButton fileType="csv" whichDownload="template" className="margin-bottom-3" />
           <h2>Complete sample data</h2>
-          <CSVExcelDownloadButton fileType="xlsx" whichDownload="example" />
-          <CSVExcelDownloadButton fileType="csv" whichDownload="example" />
+          <CSVExcelDownloadButton fileType="xlsx" whichDownload="example" className="margin-bottom-3" />
+          <CSVExcelDownloadButton fileType="csv" whichDownload="example" className="margin-bottom-3" />
           <h2>Incomplete sample data</h2>
+          <CSVExcelDownloadButton fileType="xlsx" whichDownload="example" className="margin-bottom-3" />
+          <CSVExcelDownloadButton fileType="csv" whichDownload="example" className="margin-bottom-3" />
         </div>
       </div>
     </div>
