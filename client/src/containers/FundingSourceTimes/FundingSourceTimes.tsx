e19dad284852b502d422a92befa14b579616225b
bamader <brandon@skylight.digital>
2020-10-30 15:16:42 -0400

    Correct table spacing

--- client/src/containers/FundingSourceTimes/FundingSourceTimes.tsx
+++ client/src/containers/FundingSourceTimes/FundingSourceTimes.tsx
@@ -1,21 +1,63 @@
 import React from 'react';
 import { Table, Column } from '@ctoec/component-library';
 import { BackButton } from '../../components/BackButton';
-import { FundingSourceTime } from '../../shared/models';
+import {
+  FundingSource,
+  FundingSourceTime,
+  FundingSpace,
+  FundingTime,
+} from '../../shared/models';
 import { FUNDING_SOURCE_TIMES } from '../../shared/constants';
 import { getH1RefForTitle } from '../../utils/getH1RefForTitle';
 
 const FundingSourceTimes: React.FC = () => {
   const h1Ref = getH1RefForTitle();
 
-  const columns: Column<FundingSourceTime>[] = [
+  /**
+   * Structure to hold simplified information needed to show Funding
+   * Spaces across columns with proper formatting.
+   */
+  type SpaceToFunding = {
+    space: FundingTime;
+    type: string;
+    sources: FundingSource[];
+    format: string;
+    numSpacesInType: number;
+    showSpace: boolean;
+  };
+
+  // Only want to display the FundingSpace row header once for each
+  // applicable block of contract spaces
+  let setSpaceHeader: boolean;
+  const spaces: SpaceToFunding[] = [];
+
+  // Need to 'invert' the array of funding_source_times because we
+  // want _contract spaces_ to be what defines a row so that the
+  // text between spaces and their formats will always match
+  FUNDING_SOURCE_TIMES.map((fst) => {
+    setSpaceHeader = false;
+    fst.fundingTimes.map((time) => {
+      const spaceFromFST: SpaceToFunding = {
+        space: time.value,
+        type: fst.displayName,
+        sources: fst.fundingSources,
+        format: time.formats.map((format) => '"' + format + '"').join(' or '),
+        numSpacesInType: fst.fundingTimes.length,
+        showSpace: !setSpaceHeader,
+      };
+      spaces.push(spaceFromFST);
+      setSpaceHeader = true;
+    });
+  });
+
+  const columns: Column<SpaceToFunding>[] = [
     {
       name: 'Funding Type',
       cell: ({ row }) =>
-        row ? (
-          <th scope="row">
-            <div className="text-bold">{row.displayName}</div>
-            <div>({row.fundingSources.join(' or ')})</div>
+        row && row.showSpace ? (
+          <th scope="row" rowSpan={row.numSpacesInType}>
+            <div className="text-bold">{row.type}</div>
+            <div>({row.sources.join(' or ')})</div>
           </th>
         ) : (
           <></>
@@ -26,13 +68,7 @@ const FundingSourceTimes: React.FC = () => {
       cell: ({ row }) =>
         row ? (
           <td>
-            {row.fundingTimes.map((fundingTime) => {
-              return (
-                <div className="margin-top-3 margin-bottom-3">
-                  {fundingTime.value}
-                </div>
-              );
-            })}
+            <div className="margin-top-3 margin-bottom-3">{row.space}</div>
           </td>
         ) : (
           <></>
@@ -40,22 +76,7 @@ const FundingSourceTimes: React.FC = () => {
     },
     {
       name: 'Accepted formats',
-      cell: ({ row }) =>
-        row ? (
-          <td>
-            {row.fundingTimes.map((fundingTime) => {
-              return (
-                <div className="margin-top-3 margin-bottom-3">
-                  {fundingTime.formats
-                    .map((format) => '"' + format + '"')
-                    .join(' or ')}
-                </div>
-              );
-            })}
-          </td>
-        ) : (
-          <></>
-        ),
+      cell: ({ row }) => (row ? <td>{row.format}</td> : <></>),
     },
   ];
 
@@ -70,8 +91,8 @@ const FundingSourceTimes: React.FC = () => {
       <div className="margin-top-4">
         <Table
           id="data-requirements-table"
-          data={FUNDING_SOURCE_TIMES}
-          rowKey={(row) => (row ? row.displayName : '')}
+          data={spaces}
+          rowKey={(row) => (row ? row.format : '')}
           columns={columns}
           defaultSortColumn={0}
         />
