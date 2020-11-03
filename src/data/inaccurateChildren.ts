f242d364b3afc89325ed0ee73a3ea40aa12d33eb
Melanie <melanie.mazanec@gmail.com>
2020-11-03 10:04:27 -0500

    Catch validation error

---
+++ src/data/inaccurateChildren.ts
@@ -0,0 +1,7 @@
+// Iterate through, delete keys arbitrarily
+const missingSomeInfo = [];
+
+// Delete one key from all children
+const allMissingOneField = [];
+
+export { missingSomeInfo, allMissingOneField };
