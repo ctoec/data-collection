ed5a1faa116e65a844bf9496f0170de4044b1ff0
Melanie <melanie.mazanec@gmail.com>
2020-11-11 08:20:50 -0500

    Add migration to change varchar limit

--- src/migration/1605050160456-IncreaseFundingTimeCharacterLimit.ts
+++ src/migration/1605050160456-IncreaseFundingTimeCharacterLimit.ts
@@ -1,14 +1,18 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class IncreaseFundingTimeCharacterLimit1605050160456 implements MigrationInterface {
+export class IncreaseFundingTimeCharacterLimit1605050160456
+  implements MigrationInterface {
+  name = 'IncreaseFundingTimeCharacterLimit1605050160456';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "funding_space" ALTER COLUMN "time" varchar(50) NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "funding_space" ALTER COLUMN "time" varchar(50) NOT NULL`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "funding_space" ALTER COLUMN "time" varchar(20) NOT NULL`);
-
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "funding_space" ALTER COLUMN "time" varchar(20) NOT NULL`
+    );
+  }
 }
