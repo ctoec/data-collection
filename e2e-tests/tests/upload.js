480f32588cdd940ecc742a029ede683185afbc5e
Melanie <melanie.mazanec@gmail.com>
2020-11-12 13:57:43 -0500

    Make notes

--- e2e-tests/tests/upload.js
+++ e2e-tests/tests/upload.js
@@ -1,5 +1,4 @@
 const { login } = require('../utils/login');
-const { navigateToRoster } = require('../utils/navigateToRoster');
 
 module.exports = {
   '@tags': ['upload'],
@@ -11,10 +10,7 @@ module.exports = {
     // Log in
     await login(browser);
     // Go to file upload
-    await browser.waitForElementVisible(
-      'xpath',
-      '//*/h1[contains(.,"Let\'s get started")]'
-    );
+    await browser.waitForElementVisible('xpath', '//*/h1[contains(.,"Let\'s get started")]');
     await browser.execute(function () {
       document.querySelector('a[href="/upload"]').click();
     });
@@ -24,14 +20,12 @@ module.exports = {
     );
 
     // Upload example data from local directory
+    // TODO: use wget in YAML to get the example file and move it into the right directory in Circle CI
     await browser.UploadLocalFile(`${process.cwd()}/Example.xlsx`, '#report');
     // TODO: change the ID on the upload element to make more sense
 
     // Accept the replace thing if there is one
-    const replaceDataButtonArgs = [
-      'xpath',
-      "//*/button[contains(.,'Replace data')]",
-    ];
+    const replaceDataButtonArgs = ['xpath', "//*/button[contains(.,'Replace data')]"];
     await browser.element(...replaceDataButtonArgs, async (result) => {
       if (result.state === 'success') {
         await browser.click(...replaceDataButtonArgs);
