c5f45cf92500260d98a4e46602b5b6cf9cdf88e6
jlhogan <julia@skylight.digital>
2020-11-12 17:55:01 -0500

    Bug fixes 11-12

--- src/controllers/children/update.ts
+++ src/controllers/children/update.ts
@@ -16,10 +16,9 @@ export const updateChild = async (
   update: Partial<Child>
 ) => {
   const readOrgIds = await getReadAccessibleOrgIds(user);
-  let child = await getManager().findOne(Child, id, {
+  const child = await getManager().findOne(Child, id, {
     where: { organization: { id: In(readOrgIds) } },
   });
-  child = removedDeletedEntitiesFromChild(child);
 
   if (!child) {
     console.warn(
