23b8a69446a01bf8f7eea5bf7bc739430f135868
Melanie <melanie.mazanec@gmail.com>
2020-11-30 13:32:13 -0500

    Finish migration to remove null defaults

--- src/migration/1606268723397-RemoveDefaultsFromChild.ts
+++ src/migration/1606268723397-RemoveDefaultsFromChild.ts
@@ -3,12 +3,30 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class RemoveDefaultsFromChild1606268723397 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "child" (
+                "raceNotDisclosed" bit,
+                "hispanicOrLatinxEthnicity" bit,
+                "dualLanguageLearner" bit,
+                "foster" bit CONSTRAINT "DF_4f4d36a381d8a984f1e0006f6e9", "receivesDisabilityServices" bit CONSTRAINT "DF_820b6edac45ec4ca39ee34fe0cf")`
+        );
+        await queryRunner.query(`ALTER TABLE "family" (
+            "homelessness" bit CONSTRAINT "DF_ff0d12e7344b70e7bf34b24f6f3"
+        )`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE "child" ("raceNotDisclosed" bit DEFAULT 1, "hispanicOrLatinxEthnicity" bit DEFAULT NULL, "gender" varchar(20), "dualLanguageLearner" bit DEFAULT NULL, "foster" bit CONSTRAINT "DF_4f4d36a381d8a984f1e0006f6e9" DEFAULT NULL, "receivesDisabilityServices" bit CONSTRAINT "DF_820b6edac45ec4ca39ee34fe0cf" DEFAULT NULL, "specialEducationServicesType" varchar(20), "familyId" int, "organizationId" int NOT NULL, "authorId" int, "updatedAt" datetime2 NOT NULL CONSTRAINT "DF_584ad2a7bdda2e07087016f7b1b" DEFAULT getdate(), CONSTRAINT "PK_4609b9b323ca37c6bc435ec4b6b" PRIMARY KEY ("id"))`
+            `ALTER TABLE "child" (
+                "raceNotDisclosed" bit DEFAULT 1,
+                "hispanicOrLatinxEthnicity" bit DEFAULT NULL,
+                "dualLanguageLearner" bit DEFAULT NULL,
+                "foster" bit CONSTRAINT "DF_4f4d36a381d8a984f1e0006f6e9" DEFAULT NULL,
+                "receivesDisabilityServices" bit CONSTRAINT "DF_820b6edac45ec4ca39ee34fe0cf" DEFAULT NULL)`
         );
+        await queryRunner.query(`ALTER TABLE "family" (
+            "homelessness" bit CONSTRAINT "DF_ff0d12e7344b70e7bf34b24f6f3" DEFAULT NULL
+        )`)
     }
 
 }
