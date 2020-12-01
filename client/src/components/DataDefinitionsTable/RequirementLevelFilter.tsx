fbc3f731dd1ec67b6646fb36a7244e4bac295a18
jlhogan <julia@skylight.digital>
2020-12-01 10:45:57 -0500

    Accessible llabelling

--- client/src/components/DataDefinitionsTable/RequirementLevelFilter.tsx
+++ client/src/components/DataDefinitionsTable/RequirementLevelFilter.tsx
@@ -8,32 +8,28 @@ const TOGGLE_OPTS = { ALL_FIELDS: 'all', REQUIRED_FIELDS: 'required' };
 export const RequirementLevelFilter: React.FC<RequirementLevelFilterProps> = ({
   setFilter,
 }) => (
-  <>
-    <div className="margin-bottom-1 margin-top-3 text-bold">Filter fields</div>
-    <RadioButtonGroup
-      className="requirement-level-filter"
-      legend="filter fields"
-      showLegend={false}
-      id="filter-fields"
-      inForm={false}
-      inputName="field filter option"
-      defaultSelectedItemId={TOGGLE_OPTS.ALL_FIELDS}
-      options={
-        [
-          {
-            text: 'All fields',
-            id: TOGGLE_OPTS.ALL_FIELDS,
-            value: TOGGLE_OPTS.ALL_FIELDS,
-            onChange: () => setFilter(false),
-          },
-          {
-            text: 'Only required  and conditionally required fields',
-            id: TOGGLE_OPTS.REQUIRED_FIELDS,
-            value: TOGGLE_OPTS.REQUIRED_FIELDS,
-            onChange: () => setFilter(true),
-          },
-        ] as RadioOption[]
-      }
-    />
-  </>
+  <RadioButtonGroup
+    className="requirement-level-filter"
+    legend="Filter fields"
+    id="filter-fields"
+    inForm={false}
+    inputName="field filter option"
+    defaultSelectedItemId={TOGGLE_OPTS.ALL_FIELDS}
+    options={
+      [
+        {
+          text: 'All fields',
+          id: TOGGLE_OPTS.ALL_FIELDS,
+          value: TOGGLE_OPTS.ALL_FIELDS,
+          onChange: () => setFilter(false),
+        },
+        {
+          text: 'Only required  and conditionally required fields',
+          id: TOGGLE_OPTS.REQUIRED_FIELDS,
+          value: TOGGLE_OPTS.REQUIRED_FIELDS,
+          onChange: () => setFilter(true),
+        },
+      ] as RadioOption[]
+    }
+  />
 );
--- client/src/components/DataDefinitionsTable/RequirementLevelLegend.tsx
+++ client/src/components/DataDefinitionsTable/RequirementLevelLegend.tsx
@@ -3,29 +3,29 @@ import { TEMPLATE_REQUIREMENT_LEVELS } from '../../shared/constants';
 import { getRequiredTag } from './utils';
 
 export const RequirementLevelLegend: React.FC = () => (
-  <>
-    <div className="margin-bottom-1 text-bold">Requirement levels</div>
-    <table className="margin-bottom-1">
-      <tbody>
-        <tr>
-          <td>{getRequiredTag(TEMPLATE_REQUIREMENT_LEVELS.REQUIRED)}</td>
-          <td>Must be entered for all enrollments</td>
-        </tr>
-        <tr>
-          <td>{getRequiredTag(TEMPLATE_REQUIREMENT_LEVELS.CONDITIONAL)}</td>
-          <td>
-            Must be entered for certain scenarios (such as for an exited
-            enrollment)
-          </td>
-        </tr>
-        <tr>
-          <td>{getRequiredTag(TEMPLATE_REQUIREMENT_LEVELS.OPTIONAL)}</td>
-          <td>
-            Suggested for better data quality and record matching, but not
-            required for submission to OEC
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </>
+  <table>
+    <caption className="text-bold text-left margin-bottom-1">
+      Requirement levels
+    </caption>
+    <tbody>
+      <tr>
+        <td>{getRequiredTag(TEMPLATE_REQUIREMENT_LEVELS.REQUIRED)}</td>
+        <td>Must be entered for all enrollments</td>
+      </tr>
+      <tr>
+        <td>{getRequiredTag(TEMPLATE_REQUIREMENT_LEVELS.CONDITIONAL)}</td>
+        <td>
+          Must be entered for certain scenarios (such as for an exited
+          enrollment)
+        </td>
+      </tr>
+      <tr>
+        <td>{getRequiredTag(TEMPLATE_REQUIREMENT_LEVELS.OPTIONAL)}</td>
+        <td>
+          Suggested for better data quality and record matching, but not
+          required for submission to OEC
+        </td>
+      </tr>
+    </tbody>
+  </table>
 );
