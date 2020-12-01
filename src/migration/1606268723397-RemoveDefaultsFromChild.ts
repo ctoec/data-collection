9ad6b9f2daaf5c8a9dcf28ad87bd4dec169181d1
Melanie <melanie.mazanec@gmail.com>
2020-12-01 10:44:24 -0500

    Abstract useful util

--- src/migration/1606268723397-RemoveDefaultsFromChild.ts
+++ src/migration/1606268723397-RemoveDefaultsFromChild.ts
@@ -1,39 +1,38 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { dropDefaultConstraint } from './utils/dropDefaultConstraint';
 
 export class RemoveDefaultsFromChild1606268723397
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const dropDefaultConstraint = async (tableName, columnName) => {
-      const _query = `(SELECT d.name FROM sys.default_constraints AS d INNER JOIN sys.columns AS c ON d.parent_object_id = c.object_id AND d.parent_column_id = c.column_id WHERE d.parent_object_id = OBJECT_ID(N'${tableName}', N'U') AND c.name = '${columnName}')`;
-      const columnConstraints = await queryRunner.query(_query);
-      if (!columnConstraints?.length) return;
-      const constraintName = columnConstraints[0].name;
-      await queryRunner.query(
-        `ALTER TABLE "${tableName}" DROP constraint "${constraintName}";`
-      );
-    };
-
     const makeBooleansIntoVarChars = async (tableName, columnName) => {
       await queryRunner.query(
         `ALTER TABLE "${tableName}" ALTER COLUMN "${columnName}" varchar(20);`
       );
     };
 
-    await dropDefaultConstraint('child', 'raceNotDisclosed');
+    await dropDefaultConstraint(queryRunner, 'child', 'raceNotDisclosed');
 
-    await dropDefaultConstraint('child', 'hispanicOrLatinxEthnicity');
+    await dropDefaultConstraint(
+      queryRunner,
+      'child',
+      'hispanicOrLatinxEthnicity'
+    );
     await makeBooleansIntoVarChars('child', 'hispanicOrLatinxEthnicity');
 
-    await dropDefaultConstraint('child', 'dualLanguageLearner');
+    await dropDefaultConstraint(queryRunner, 'child', 'dualLanguageLearner');
     await makeBooleansIntoVarChars('child', 'dualLanguageLearner');
 
-    await dropDefaultConstraint('child', 'foster');
+    await dropDefaultConstraint(queryRunner, 'child', 'foster');
     await makeBooleansIntoVarChars('child', 'foster');
 
-    await dropDefaultConstraint('child', 'receivesDisabilityServices');
+    await dropDefaultConstraint(
+      queryRunner,
+      'child',
+      'receivesDisabilityServices'
+    );
     await makeBooleansIntoVarChars('child', 'receivesDisabilityServices');
 
-    await dropDefaultConstraint('family', 'homelessness');
+    await dropDefaultConstraint(queryRunner, 'family', 'homelessness');
     await makeBooleansIntoVarChars('family', 'homelessness');
   }
 
