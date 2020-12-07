2cacc00da564bae6dc2b4c0b80625beded3932c3
Melanie <melanie.mazanec@gmail.com>
2020-12-07 11:53:08 -0500

    Make notes

--- e2e-tests/tests/deleteChild.js
+++ e2e-tests/tests/deleteChild.js
@@ -18,17 +18,15 @@ module.exports = {
     await login(browser);
     await navigateToRoster(browser);
     await clickOnChildInRoster(browser);
+    // TODO: if no child, upload file
+    // Before all tests, curl to get the latest example locally and name it Example.xlsx
+    // Change example file to only be like 10
+    // Random seed to keep it consistent?
 
     await browser.click('xpath', "//*/button[contains(., 'Delete record')]");
-    await browser.click(
-      'xpath',
-      "//*/button[contains(., 'Yes, delete record')]"
-    );
+    await browser.click('xpath', "//*/button[contains(., 'Yes, delete record')]");
     // TODO: change if we change alert header level
-    await browser.waitForElementVisible(
-      'xpath',
-      "//*/h2[contains(., 'Record deleted')]"
-    );
+    await browser.waitForElementVisible('xpath', "//*/h2[contains(., 'Record deleted')]");
 
     // TODO: make sure the record deleted was the right one
     browser.end();
