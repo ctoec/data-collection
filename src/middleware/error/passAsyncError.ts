ccd203447388fa2f87d159e88cf4ddbbdf69b6ab
jlhogan <julia@skylight.digital>
2020-12-14 08:39:17 -0500

    Fix enrollment update without site (#915)
    * Fix enrollment update without site
    
    * Rider to fixup the circleci post-e2e deploy to devsecure
    
    * Wtf
    
    * Fix y fixy

--- src/middleware/error/passAsyncError.ts
+++ src/middleware/error/passAsyncError.ts
@@ -1,18 +1,18 @@
 import { RequestHandler, Request, Response, NextFunction } from 'express';
 
 /**
+ * Pretty much:
+ * https://github.com/Abazhenov/express-async-handler/blob/master/index.js
+ *
  * To avoid having to write the same try/catch logic
  * in every single async middleware function,
  * wrap them in this to handle passing promise rejections
- * and throw exceptions along to the error handling middleware
+ * and thrown exceptions along to the error handling middleware
  *
- * See `routes/enrollmentReport.ts` for an example.
- * @param func
+ * @param func The request handler function
  */
 export function passAsyncError(func: RequestHandler) {
-  return function (req: Request, res: Response, next: NextFunction) {
-    // @ts-ignore
-    // This works the way it needs to so I'm not sure why typescript is complaining
-    func(req, res, next).catch(next);
+  return async function (req: Request, res: Response, next: NextFunction) {
+    Promise.resolve(func(req, res, next)).catch(next);
   };
 }
