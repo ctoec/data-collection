b37428be3b4d99f24c1832263aa12b9be45aabc7
Melanie <melanie.mazanec@gmail.com>
2020-12-18 21:24:19 -0500

    Cleanup

--- client/src/components/Forms/FamilyIncome/Form.tsx
+++ client/src/components/Forms/FamilyIncome/Form.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   Child,
   IncomeDetermination,
@@ -22,7 +23,6 @@ import {
 import { FosterIncomeNotRequiredAlert } from './FosterIncomeNotRequiredAlert';
 import useIsMounted from '../../../hooks/useIsMounted';
 import { useValidationErrors } from '../../../hooks/useValidationErrors';
-import { useLocation } from 'react-router-dom';
 
 const incomeDeterminationFields = [
   'numberOfPeople',
@@ -52,9 +52,9 @@ export const doesFamilyIncomeFormHaveErrors = (
   const incomeDeterminationsHaveError = child?.family?.incomeDeterminations
     ?.length
     ? !!getValidationStatusForFields(
-        child.family.incomeDeterminations,
-        incomeDeterminationFields
-      )
+      child.family.incomeDeterminations,
+      incomeDeterminationFields
+    )
     : false;
 
   return familyHasIncomeDeterminationError || incomeDeterminationsHaveError;
@@ -80,7 +80,6 @@ export const FamilyIncomeForm: React.FC<FamilyIncomeFormProps> = ({
   if (!child?.family) {
     throw new Error('Family income form rendered without family');
   }
-  console.log(child.family);
   const [loading, setLoading] = useState(false);
   const { accessToken } = useContext(AuthenticationContext);
   const { errorsHidden, setErrorsHidden } = useValidationErrors(
--- e2e-tests/utils/enterFormValue.js
+++ e2e-tests/utils/enterFormValue.js
@@ -2,7 +2,7 @@ const { scrollToElement } = require('./scrollToElement');
 
 module.exports = {
   enterFormValue: async function (browser, id, input) {
-    const selectorArgs = ['css selector', `[id^="${id}"]`];
+    const selectorArgs = ['css selector', `#${id}`];
     await scrollToElement(browser, selectorArgs);
     await browser.clearValue(...selectorArgs);
     await browser.setValue(...selectorArgs, input);
@@ -10,14 +10,14 @@ module.exports = {
   },
   clickFormEl: async function (browser, id, opts) {
     const { clickLabel, clickChildIndex } = opts || {};
-    const selectorArgs = ['css selector', `[id^="${id}"]`];
+    const selectorArgs = ['css selector', `#${id}`];
     if (clickLabel) {
       selectorArgs[1] = `label[for*=${id}]`;
     }
     await scrollToElement(browser, selectorArgs);
     if (clickChildIndex) {
       // Used for select/option
-      selectorArgs[1] = `[id^="${id}"] *:nth-child(${clickChildIndex})`;
+      selectorArgs[1] = `#${id} *:nth-child(${clickChildIndex})`;
     }
     await browser.click(...selectorArgs);
     await browser.pause(1000); // Wait for change... annoying but better than flaky tests
