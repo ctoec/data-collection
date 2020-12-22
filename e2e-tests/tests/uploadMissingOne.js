aa3eb514f0213a42f2ef7848383e39d9843b90f9
bamader <49412165+bamader@users.noreply.github.com>
2020-12-22 08:32:21 -0500

    Split upload (#953)
    * Incomplete upload coverage in e2e
    
    * Finish incomplete data test
    
    * Fix forgetfulness
    
    * Change tag
    
    * Update uploadMissingOne.js
    
    * Update uploadMissingSome.js

---
+++ e2e-tests/tests/uploadMissingOne.js
@@ -0,0 +1,13 @@
+const { login } = require('../utils/login');
+const { uploadFile } = require('../utils/uploadFile');
+
+module.exports = {
+  '@tags': ['upload'],
+  uploadMissingOne: async function (browser) {
+    await browser.init();
+    await browser.timeoutsImplicitWait(10000);
+    await login(browser);
+    await uploadFile(browser, 'missingOne');
+    browser.end();
+  },
+};
