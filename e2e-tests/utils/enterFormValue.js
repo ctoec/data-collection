2d7f815bafdc43f8e675542f984e4176a77f1e8f
Melanie <melanie.mazanec@gmail.com>
2020-12-30 19:15:47 -0500

    Expect one error

--- e2e-tests/utils/enterFormValue.js
+++ e2e-tests/utils/enterFormValue.js
@@ -22,19 +22,5 @@ module.exports = {
     await browser.click(...selectorArgs);
     await browser.pause(1000); // Wait for change... annoying but better than flaky tests
   },
-  enterFormSection: async function (browser, setOfFields) {
-    // To enter one of the sections in nweChildInput
-    for (let i = 0; i < setOfFields.length; i++) {
-      const field = setOfFields[i];
-      const { id, newValue } = field;
-      if (newValue) {
-        // If the value is specified, set the value
-        await enterFormValue(browser, id, newValue);
-      } else {
-        // Otherwise click on it
-        await clickFormEl(browser, id, field);
-      }
-    }
-  },
   // TODO: keyboard interactivity
 };
