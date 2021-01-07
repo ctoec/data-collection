751c9559e7cb126acfec1fd52176cb9f08210e97
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2021-01-07 14:02:34 -0500

    Use name formatter (#987)
    Use name formatter

--- client/src/containers/BatchEdit/BatchEditItemContent.tsx
+++ client/src/containers/BatchEdit/BatchEditItemContent.tsx
@@ -124,7 +124,10 @@ export const BatchEditItemContent: React.FC<BatchEditItemContentProps> = ({
         <InlineIcon icon="complete" />
       </div>
       <div className="text-center text-bold font-body-lg">
-        {`${child?.firstName}'s record is now complete!`}
+        {`${nameFormatter(child, {
+          firstOnly: true,
+          capitalize: true,
+        })}'s record is now complete!`}
       </div>
     </div>
   );
@@ -138,7 +141,9 @@ export const BatchEditItemContent: React.FC<BatchEditItemContentProps> = ({
     <>
       <div className="padding-left-2 padding-right-2 padding-bottom-3">
         <div className="display-flex flex-row flex-justify flex-align-end">
-          <h2 className="margin-bottom-0">{nameFormatter(child)}</h2>
+          <h2 className="margin-bottom-0">
+            {nameFormatter(child, { lastNameFirst: true, capitalize: true })}
+          </h2>
           <div className="text-baseline text-base">
             Date of birth: {child.birthdate?.format('MM/DD/YYYY') || 'Missing'}
           </div>
