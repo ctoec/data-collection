cab28540d98322c672463260a6fcc8d065e0101a
jlhogan <julia@skylight.digital>
2021-01-07 16:11:16 -0500

    Adds values for existing income dets (#1002)

--- src/migration/1608582587797-AddIncomeNotDisclosedToFamily.ts
+++ src/migration/1608582587797-AddIncomeNotDisclosedToFamily.ts
@@ -1,4 +1,5 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { dropDefaultConstraint } from './utils/dropDefaultConstraint';
 
 export class AddIncomeNotDisclosedToFamily1608582587797
   implements MigrationInterface {
@@ -6,11 +7,16 @@ export class AddIncomeNotDisclosedToFamily1608582587797
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "income_determination" ADD "incomeNotDisclosed" bit`
+      `ALTER TABLE "income_determination" ADD "incomeNotDisclosed" bit DEFAULT(0) WITH VALUES`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await dropDefaultConstraint(
+      queryRunner,
+      'income_determination',
+      'incomeNotDisclosed'
+    );
     await queryRunner.query(
       `ALTER TABLE "income_determination" DROP COLUMN "incomeNotDisclosed"`
     );
