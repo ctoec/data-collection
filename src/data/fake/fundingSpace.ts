5f9af1923afa95141d6038bea54745d6bcfb3c7c
Jordan Guinn <jordan@skylight.digital>
2021-01-20 09:35:07 -0800

    right

--- src/data/fake/fundingSpace.ts
+++ src/data/fake/fundingSpace.ts
@@ -2,12 +2,13 @@ import { random } from 'faker';
 import { FundingSpace, Organization } from '../../entity';
 import { AgeGroup, FundingSource } from '../../../client/src/shared/models';
 import { FUNDING_SOURCE_TIMES } from '../../../client/src/shared/constants';
+import { FUNDING_SOURCES_WITH_ACRONYMS } from '../../template/constants';
 
 export const getFakeFundingSpaces = (
   organization: Organization
 ): FundingSpace[] => {
   let fundingSpaces = [];
-  Object.values(FundingSource).forEach((source) => {
+  Object.entries(FundingSource).forEach(([sourceKey, source]) => {
     FUNDING_SOURCE_TIMES.filter((fst) =>
       fst.fundingSources.includes(source)
     ).forEach((match) => {
@@ -18,7 +19,8 @@ export const getFakeFundingSpaces = (
             id: fundingSpaces.length + 1,
             capacity: source === FundingSource.SHS ? -1 : random.number(50),
             organization,
-            source,
+            // Funding sources in example upload files should include acronyms
+            source: FUNDING_SOURCES_WITH_ACRONYMS[sourceKey],
             ageGroup,
             time: fundingTime.value,
             organizationId: organization.id,
