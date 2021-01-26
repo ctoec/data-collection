eefe0dcbac5e9d2f61dd1d79cf0aef263b26dbe8
Melanie <melanie.mazanec@gmail.com>
2021-01-26 12:01:32 -0500

    Start to refactor map func

--- client/src/containers/Home/mapFundingSpacesToCards.tsx
+++ client/src/containers/Home/mapFundingSpacesToCards.tsx
@@ -5,43 +5,40 @@ import { NestedFundingSpaces } from '../../shared/payloads/NestedFundingSpaces';
 // Map each calculated funding space distribution into a card
 // element that we can format for display
 export const mapFundingSpacesToCards = (
-  fundingSpacesDisplay: NestedFundingSpaces
-) => {
-  const fundingCards = (fundingSpacesDisplay || []).map((fsd: any) => (
-    <div className="desktop:grid-col-4 three-column-card">
-      <Card>
-        <div className="padding-0">
-          <h3 className="margin-top-0">{fsd.sourceName}</h3>
-          {fsd.includedAgeGroups.map((ag: any) => (
+  nestedSpaces: NestedFundingSpaces
+) => Object.keys(nestedSpaces).map((source) => (
+  <div className="desktop:grid-col-4 three-column-card">
+    <Card>
+      <div className="padding-0">
+        <h3 className="margin-top-0">{source}</h3>
+        {Object.keys(nestedSpaces[source]).map((ag: any) => (
+          <>
+            <div>
+              <p className="text-bold">{ag.ageGroup}</p>
+            </div>
             <>
-              <div>
-                <p className="text-bold">{ag.ageGroup}</p>
-              </div>
-              <>
-                {ag.includedTimes.map((t: any) => {
-                  // Account for fundings that have negative capacities stored
-                  // Also hide capacities for SR fundings, since we're less
-                  // confident on those and they change a lot
-                  let spaceNumbers = `${t.filled}`;
-                  if (
-                    t.capacity !== -1 &&
-                    !fsd.sourceName.includes('School Readiness')
-                  )
-                    spaceNumbers += `/${t.capacity} `;
-                  else spaceNumbers += ` `;
-                  return (
-                    <p className="text-base-darker padding-0 margin-0">
-                      <b>{spaceNumbers}</b>
-                      {` ${t.timeType}`}
-                    </p>
-                  );
-                })}
-              </>
+              {ag.includedTimes.map((t: any) => {
+                // Account for fundings that have negative capacities stored
+                // Also hide capacities for SR fundings, since we're less
+                // confident on those and they change a lot
+                let spaceNumbers = `${t.filled}`;
+                if (
+                  t.capacity !== -1 &&
+                  !fsd.sourceName.includes('School Readiness')
+                )
+                  spaceNumbers += `/${t.capacity} `;
+                else spaceNumbers += ` `;
+                return (
+                  <p className="text-base-darker padding-0 margin-0">
+                    <b>{spaceNumbers}</b>
+                    {` ${t.timeType}`}
+                  </p>
+                );
+              })}
             </>
-          ))}
-        </div>
-      </Card>
-    </div>
-  ));
-  return fundingCards;
-};
+          </>
+        ))}
+      </div>
+    </Card>
+  </div>
+));
