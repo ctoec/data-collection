5f66814519e3b799724f418539a469cc913817e6
Jordan Guinn <jordan@skylight.digital>
2021-02-02 23:24:25 -0800

    byeeeeee

--- src/entity/decorators/Funding/fundingOverlapValidation.ts
+++ src/entity/decorators/Funding/fundingOverlapValidation.ts
@@ -43,7 +43,6 @@ export function FundingDoesNotOverlap(
             const _firstPeriod = _funding.firstReportingPeriod;
 
             if (!_firstPeriod) {
-              console.error('AH HEY LOOK AT DIS', _funding);
               return;
             }
 
