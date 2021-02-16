7ee28bb6fd42206ed9448dd8196ac154fe9302cc
bamader <brandon@skylight.digital>
2021-02-16 12:27:59 -0500

    Add change tag enum

---
+++ client/src/shared/models/ChangeTags.ts
@@ -0,0 +1,9 @@
+export enum ChangeTag {
+  agedUp = 'Aged Up',
+  changedEnrollment = 'Changed Enrollment',
+  changedFunding = 'Changed Funding',
+  newRecord = 'New Record',
+  withdrawnRecord = 'Withdrawn Record',
+  incomeDet = 'New Income Determination',
+  edited = 'Edited Information',
+}
