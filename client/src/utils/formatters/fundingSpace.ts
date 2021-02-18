04c10c9d401f011d60b5d2eb0f64733a1e43567b
Jordan Guinn <jordan@skylight.digital>
2021-02-17 23:05:31 -0800

    SEE YA

--- client/src/utils/formatters/fundingSpace.ts
+++ client/src/utils/formatters/fundingSpace.ts
@@ -1,32 +1,18 @@
-import {
-  FundingSpace,
-  FundingTime,
-  FundingTimeSplit,
-} from '../../shared/models';
+import { FundingSpace, FundingTime } from '../../shared/models';
 
 export const fundingSpaceFormatter = (fundingSpace: FundingSpace) => {
-  const FULL_YEAR_WEEKS = 52;
   if (fundingSpace.time !== FundingTime.SplitTime) {
-    return `${fundingSpace.time} (${FULL_YEAR_WEEKS} weeks)`;
+    return fundingSpace.time;
   }
 
   // Handle "split-time" funding spaces (CDC only)
-  const timeSplit = fundingSpace.timeSplit || ({} as FundingTimeSplit);
-  const fullTimeWeeks = timeSplit.fullTimeWeeks;
-  const partTimeWeeks = timeSplit.partTimeWeeks;
-  if (!fullTimeWeeks || !partTimeWeeks) {
-    // TODO: change this once we have this data
-    console.warn(
-      'Part time full time split display needs to be updated when data is available'
-    );
+  const fullTimeWeeks = fundingSpace.timeSplit?.fullTimeWeeks;
+  const partTimeWeeks = fundingSpace.timeSplit?.partTimeWeeks;
+
+  // TODO: change this once we have this data
+  if (!fullTimeWeeks || !partTimeWeeks || partTimeWeeks <= fullTimeWeeks) {
     return `${FundingTime.PartTime} / ${FundingTime.FullTime}`;
+  } else {
+    return `${FundingTime.FullTime} / ${FundingTime.PartTime}`;
   }
-
-  const fullTimeFirst = fullTimeWeeks > partTimeWeeks;
-
-  if (fullTimeFirst) {
-    return `${FundingTime.FullTime} (${fullTimeWeeks} weeks) / ${FundingTime.PartTime} (${partTimeWeeks} weeks)`;
-  }
-
-  return `${FundingTime.PartTime} (${partTimeWeeks} weeks) / ${FundingTime.FullTime} (${fullTimeWeeks} weeks)`;
 };
