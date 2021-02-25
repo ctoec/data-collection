e2f9ee340c4b62034e096ee828d40b20c938d6cc
Jordan Guinn <jordan@skylight.digital>
2021-02-24 22:15:14 -0800

    Revert "maybe I gotta revert this, smh"
    This reverts commit ed13320f3759e4fda33bb88babba1f8f73d40f5b.

--- client/src/containers/Upload/Upload.tsx
+++ client/src/containers/Upload/Upload.tsx
@@ -45,31 +45,35 @@ const Upload: React.FC = () => {
   // Check the file for errors if there is a file
   const [file, setFile] = useState<File>();
   useEffect(() => {
-    if (!file) return;
+    (async function submitUpload() {
+      if (!file) return;
 
-    setLoading(true);
+      setLoading(true);
+      try {
+        const formData = getFormDataBlob(file);
+        const enrollmentColumnErrors: EnrollmentColumnError[] = await apiPost(
+          `enrollment-reports/check`,
+          formData,
+          {
+            accessToken,
+            headers: { 'content-type': formData.type },
+            rawBody: true,
+          }
+        );
 
-    const formData = getFormDataBlob(file);
-    apiPost(`enrollment-reports/check`, formData, {
-      accessToken,
-      headers: { 'content-type': formData.type },
-      rawBody: true,
-    })
-      .then((enrollmentColumnErrors: EnrollmentColumnError[]) => {
         history.push('/missing-info', {
           enrollmentColumnErrors,
           file,
         });
-      })
-      .catch((error) => {
+      } catch (error) {
         handleJWTError(history, (e) => {
           setError(e);
           clearFile();
-        });
-      })
-      .finally(() => {
+        })(error);
+      } finally {
         setLoading(false);
-      });
+      }
+    })();
   }, [file]);
 
   const [fileKey, setFileKey] = useState(0);
