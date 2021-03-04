71b74cb281e42fcafe183b5dbae25e0d45c2a906
Jordan Guinn <jordan@skylight.digital>
2021-03-03 19:20:11 -0800

    update upload file e2e tests

--- e2e-tests/utils/uploadFile.js
+++ e2e-tests/utils/uploadFile.js
@@ -6,81 +6,115 @@ const {
 } = require('../utils/downloadFileToTestRunnerHost');
 
 module.exports = {
-  uploadFile: async function (
-    browser,
-    filetype,
-    whichFile = 'complete',
-    waitForHello = true
-  ) {
-    // Set await browser.timeoutsImplicitWait(10000); in the test right after browser.init for this function to work
-    const FILE_PATH = `${process.cwd()}/upload.csv`;
+  uploadFile,
+};
 
-    // Pick the download url based on which kind of upload test we want to run
-    let DOWNLOAD_URL = `${launch_url}`;
-    if (whichFile === 'complete') {
-      DOWNLOAD_URL += `/api/template/example/${filetype}`;
-    } else if (whichFile === FakeChildrenTypes.MISSING_SOME) {
-      DOWNLOAD_URL += `/api/template/example/${filetype}?whichFakeChildren=missingSome`;
-    } else if (whichFile === FakeChildrenTypes.MISSING_ONE) {
-      DOWNLOAD_URL += `/api/template/example/${filetype}?whichFakeChildren=missingOne`;
-    } else if (whichFile === FakeChildrenTypes.MISSING_OPTIONAL) {
-      DOWNLOAD_URL += `/api/template/example/${filetype}?whichFakeChildren=missingOptional`;
-    } else if (whichFile === FakeChildrenTypes.MISSING_CONDITIONAL) {
-      DOWNLOAD_URL += `/api/template/example/${filetype}?whichFakeChildren=missingConditional`;
-    }
+async function uploadFile(
+  browser,
+  fileType,
+  whichFile = 'complete',
+  waitForHello = true
+) {
+  // Set await browser.timeoutsImplicitWait(10000); in the test right after browser.init for this function to work
+  const FILE_PATH = `${process.cwd()}/upload.csv`;
 
-    const isCompleteTestRun = whichFile === 'complete';
-    const isMissingOptionalRun =
-      whichFile === FakeChildrenTypes.MISSING_OPTIONAL;
+  const downloadUrl = getDownloadUrl(whichFile, fileType);
 
-    await downloadFileToTestRunnerHost(FILE_PATH, DOWNLOAD_URL);
+  const isCompleteTestRun = whichFile === 'complete';
+  const isMissingOptionalRun = whichFile === FakeChildrenTypes.MISSING_OPTIONAL;
 
-    // Go to file upload
-    if (waitForHello) {
-      await browser.waitForElementVisible(
-        'xpath',
-        '//*/h1[contains(.,"Hello Voldemort")]'
-      );
-    }
-    await browser.execute(function () {
-      document.querySelector('a[href="/upload"]').click();
-    });
+  await downloadFileToTestRunnerHost(FILE_PATH, downloadUrl);
+
+  // Go to file upload
+  if (waitForHello) {
     await browser.waitForElementVisible(
       'xpath',
-      '//*/h1[contains(text(),"Upload your enrollment data")]'
+      '//*/h1[contains(.,"Hello Voldemort")]'
     );
+  }
 
-    // Upload example data from local directory
-    await browser.UploadLocalFile(FILE_PATH, '#report');
-    await browser.pause(5000);
+  await startUpload(browser, FILE_PATH);
+  await acceptModal(
+    browser,
+    'Upload and correct in roster',
+    'No error modal appearing',
+    !(isCompleteTestRun || isMissingOptionalRun)
+  );
 
-    // Accept the error modal if it pops up
-    await acceptModal(
-      browser,
-      'Upload and correct in roster',
-      'No error modal appearing',
-      !(isCompleteTestRun || isMissingOptionalRun)
+  await reviewMissingInfo(browser);
+  await completeUpload(browser);
+
+  // Resulting alert will depend on whether we uploaded complete or incomplete data
+  if (isCompleteTestRun) {
+    await browser.waitForElementVisible(
+      'xpath',
+      `//*/p[contains(text(),"20 new records")]`
     );
+  } else if (isMissingOptionalRun) {
+    await browser.waitForElementVisible(
+      'xpath',
+      `//*/p[contains(text(),"10 new records")]`
+    );
+  } else {
+    await browser.waitForElementVisible(
+      'xpath',
+      `//*/h2[contains(.,'Update your roster')]`
+    );
+  }
+}
 
-    // Accept the replace thing if there is one
-    await acceptModal(browser, 'Replace data', 'No replace data button');
+//////////////////////////////////////////////////////////////////////////
 
-    // Resulting alert will depend on whether we uploaded complete or incomplete data
-    if (isCompleteTestRun) {
-      await browser.waitForElementVisible(
-        'xpath',
-        `//*/p[contains(text(),"20 children enrolled")]`
-      );
-    } else if (isMissingOptionalRun) {
-      await browser.waitForElementVisible(
-        'xpath',
-        `//*/p[contains(text(),"10 children enrolled")]`
-      );
-    } else {
-      await browser.waitForElementVisible(
-        'xpath',
-        `//*/h2[contains(.,'Update your roster')]`
-      );
-    }
-  },
-};
+async function startUpload(browser, filePath) {
+  await browser.execute(function () {
+    document.querySelector('a[href="/upload"]').click();
+  });
+  await browser.waitForElementVisible(
+    'xpath',
+    '//*/h1[contains(text(),"Upload your enrollment data")]'
+  );
+
+  await browser.UploadLocalFile(filePath, '#report');
+  await browser.pause(5000);
+}
+
+async function reviewMissingInfo(browser) {
+  await browser.waitForElementVisible(
+    'xpath',
+    `//*/h2[contains(text(),'Review missing info')]`
+  );
+
+  await browser.click('xpath', "//*/button[contains(text(), 'Next')]");
+  await browser.pause(15000);
+}
+
+async function completeUpload(browser) {
+  await browser.waitForElementVisible(
+    'xpath',
+    `//*/h2[contains(.,'Preview changes and upload file')]`
+  );
+
+  await browser.click(
+    'xpath',
+    "//*/button[contains(text(), 'Save changes to roster')]"
+  );
+  await browser.pause(5000);
+}
+
+function getDownloadUrl(whichFile, fileType) {
+  let DOWNLOAD_URL = `${launch_url}`;
+
+  if (whichFile === 'complete') {
+    DOWNLOAD_URL += `/api/template/example/${fileType}`;
+  } else if (whichFile === FakeChildrenTypes.MISSING_SOME) {
+    DOWNLOAD_URL += `/api/template/example/${fileType}?whichFakeChildren=missingSome`;
+  } else if (whichFile === FakeChildrenTypes.MISSING_ONE) {
+    DOWNLOAD_URL += `/api/template/example/${fileType}?whichFakeChildren=missingOne`;
+  } else if (whichFile === FakeChildrenTypes.MISSING_OPTIONAL) {
+    DOWNLOAD_URL += `/api/template/example/${fileType}?whichFakeChildren=missingOptional`;
+  } else if (whichFile === FakeChildrenTypes.MISSING_CONDITIONAL) {
+    DOWNLOAD_URL += `/api/template/example/${fileType}?whichFakeChildren=missingConditional`;
+  }
+
+  return DOWNLOAD_URL;
+}
