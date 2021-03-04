48816b3fb8a95ca10788ef2dd89738bc75a4cc38
Jordan Guinn <jordan@skylight.digital>
2021-03-03 21:24:43 -0800

    just forget it

--- e2e-tests/nightwatch.conf.js
+++ e2e-tests/nightwatch.conf.js
@@ -20,7 +20,7 @@ https: nightwatch_config = {
   test_settings: {
     default: {
       globals: {
-        waitForConditionTimeout: 60000, // sometimes internet is slow so wait.
+        waitForConditionTimeout: 5000, // sometimes internet is slow so wait.
       },
       desiredCapabilities: {
         browserName: 'chrome',
