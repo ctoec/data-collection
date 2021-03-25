9b3d318ef472acaa04d68b3b0a2a9874bf027617
vonbearshark <jjnaughton93@gmail.com>
2021-03-25 17:23:18 -0400

    Fixed misc test types

--- client/src/components/Forms/ChildInfo/ChildInfo.test.tsx
+++ client/src/components/Forms/ChildInfo/ChildInfo.test.tsx
@@ -4,13 +4,14 @@ import {
   accessibilityTestHelper,
 } from '../../../testHelpers';
 import { ChildInfoForm } from './Form';
-import { Family } from '../../../shared/models';
+import { Family, Organization } from '../../../shared/models';
 
 const child = {
   id: '00000000-0000-0000-0000-000000000000',
   firstName: 'First',
   lastName: 'Last',
   family: {} as Family,
+  organization: {} as Organization,
 };
 
 describe('EditRecord', () => {
