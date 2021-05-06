38297054f8aa595cf0730c6b936f4ef9bbb70daf
nickbristow <richard.nick.bristow@gmail.com>
2021-05-06 15:33:50 -0400

    Use api get

--- client/src/containers/Organizations/Organizations.tsx
+++ client/src/containers/Organizations/Organizations.tsx
@@ -1,16 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import AuthenticationContext from '../../contexts/AuthenticationContext/AuthenticationContext';
 import { Redirect } from 'react-router-dom';
 import UserContext from '../../contexts/UserContext/UserContext';
 import { useAuthenticatedSWR } from '../../hooks/useAuthenticatedSWR';
 import { OrganizationSummary } from '../../shared/payloads/OrganizationsResponse';
 import { getH1RefForTitle } from '../../utils/getH1RefForTitle';
 import { OrganizationsTable } from './OrganizationsTable';
+import { apiGet } from '../../utils/api';
 
 const Organizations: React.FC = () => {
   const { user } = useContext(UserContext);
   const h1Ref = getH1RefForTitle();
+  const { accessToken } = useContext(AuthenticationContext);
 
-  const { data } = useAuthenticatedSWR<OrganizationSummary[]>('organizations');
+  const [data, setData] = useState<OrganizationSummary[]>([]);
+  apiGet(`organizations/`, accessToken).then((res: OrganizationSummary[]) => {
+    setData(res);
+  });
 
   return !user?.isAdmin ? (
     <Redirect to="/home" />
