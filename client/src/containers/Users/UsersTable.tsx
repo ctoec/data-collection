9f8b44d1ebfb3f48b2fceff031cec97688b04451
nickbristow <richard.nick.bristow@gmail.com>
2021-05-11 10:36:32 -0400

    small refactor

--- client/src/containers/Users/UsersTable.tsx
+++ client/src/containers/Users/UsersTable.tsx
@@ -18,31 +18,26 @@ export const UsersTable: React.FC<UserTableProps> = ({ users = [] }) => (
   />
 );
 
-const TableColumns: () => Column<UserSummary>[] = () => {
-  const columns: Column<UserSummary>[] = [
-    {
-      name: 'Name',
-      sort: (row) => row.name,
-      cell: ({ row }) => (
-        <th scope="row">
-          {/* Deactivate links until org detail pages are built */}
-          {/* <Link to={`organization/${row.id}`}>{row.providerName}</Link> */}
-          {row.name}
-        </th>
-      ),
-    },
-    {
-      name: 'Email address',
-      className: 'text-no-wrap',
-      sort: (row) => (row.email ? row.email : ''),
-      cell: ({ row }) => <td>{row.email ? row.email : ''}</td>,
-    },
-    {
-      name: 'Organization(s)',
-      // sort: (row) => row.fundingSpaces.length,
-      cell: ({ row }) => <td className="maxw-card-lg">{row.organizations}</td>,
-    },
-  ];
-
-  return columns;
-};
+const TableColumns: () => Column<UserSummary>[] = () => [
+  {
+    name: 'Name',
+    sort: (row) => row.name,
+    cell: ({ row }) => (
+      <th scope="row">
+        {/* Deactivate links until org detail pages are built */}
+        {/* <Link to={`organization/${row.id}`}>{row.name}</Link> */}
+        {row.name}
+      </th>
+    ),
+  },
+  {
+    name: 'Email address',
+    className: 'text-no-wrap',
+    sort: (row) => row.email ?? '',
+    cell: ({ row }) => <td>{row.email ?? ''}</td>,
+  },
+  {
+    name: 'Organization(s)',
+    cell: ({ row }) => <td className="maxw-card-lg">{row.organizations}</td>,
+  },
+];
