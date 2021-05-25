292e04561e899978ff6efff5190e04faf3e6214c
Jordan Guinn <jordan@skylight.digital>
2021-05-25 10:25:35 -0700

    I mean... sigh

--- e2e-tests/utils/navigateToRoster.js
+++ e2e-tests/utils/navigateToRoster.js
@@ -4,12 +4,7 @@ module.exports = {
   navigateToRoster: async function (browser) {
     await makeSureNavIsOpen(browser);
     await browser.click('xpath', "//*/a[contains(@href,'/roster')]");
-    // Use only "enrolled at" to catch the case where there's 1 child
-    // and the string "children" is not in the header!
-    const rosterHeaderArgs = [
-      'xpath',
-      '//*/p[contains(text(),"enrolled at ")]',
-    ];
-    await browser.waitForElementVisible(...rosterHeaderArgs);
+
+    await browser.waitForElementVisible('xpath', '//*/p[contains(text(),"allows you to view and update the enrollment data you submitted previously")]');
   },
 };
