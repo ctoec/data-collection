873d8bae9ff62b00ab15d45466238d541a590143
Jordan Guinn <jordan@skylight.digital>
2021-05-27 12:31:11 -0700

    smh

--- client/src/shared/payloads/TemplateMetadataResponse.ts
+++ client/src/shared/payloads/TemplateMetadataResponse.ts
@@ -2,7 +2,7 @@ import { Moment } from 'moment';
 import { ColumnMetadata } from '../models';
 
 export interface TemplateMetadataResponse {
-  version: 1;
+  version: 1 | 2;
   lastUpdated: Moment;
   columnMetadata: ColumnMetadata[];
 }
